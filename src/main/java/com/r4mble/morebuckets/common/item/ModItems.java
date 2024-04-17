c9f436955e1fb1c0276de2ad3b49313f6adf0137
R4mBLe_ <misharad2008@gmail.com>
2024-04-17 17:24:08 +0300

    final update

--- src/main/java/com/r4mble/morebuckets/common/item/ModItems.java
+++ src/main/java/com/r4mble/morebuckets/common/item/ModItems.java
@@ -12,26 +12,26 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MoreBuckets.MOD_ID);
 
     public static final RegistryObject<Item> WOODEN_BUCKET = ITEMS.register("wooden_bucket", () -> new UniversalBucketItem(
-            new UniversalBucketItem.Properties().tab(ModItemTabs.MORE_BUCKETS_MOD_TAB).durability(10).upperCrackingTemperature(1000)
+            new UniversalBucketItem.Properties().tab(ModItemTabs.MORE_BUCKETS_MOD_TAB).durability(6).upperCrackingTemperature(1000)
     ));
     public static final RegistryObject<Item> STONE_BUCKET = ITEMS.register("stone_bucket", () -> new UniversalBucketItem(
-            new UniversalBucketItem.Properties().tab(ModItemTabs.MORE_BUCKETS_MOD_TAB).durability(50).upperCrackingTemperature(1000)
+            new UniversalBucketItem.Properties().tab(ModItemTabs.MORE_BUCKETS_MOD_TAB).durability(10).upperCrackingTemperature(1000)
     ));
     public static final RegistryObject<Item> GOLD_BUCKET = ITEMS.register("gold_bucket", () -> new UniversalBucketItem(
-            new UniversalBucketItem.Properties().tab(ModItemTabs.MORE_BUCKETS_MOD_TAB).durability(100)
+            new UniversalBucketItem.Properties().tab(ModItemTabs.MORE_BUCKETS_MOD_TAB).durability(0)
     ));
     public static final RegistryObject<Item> DIAMOND_BUCKET = ITEMS.register("diamond_bucket", () -> new UniversalBucketItem(
-            new UniversalBucketItem.Properties().tab(ModItemTabs.MORE_BUCKETS_MOD_TAB).durability(250)
+            new UniversalBucketItem.Properties().tab(ModItemTabs.MORE_BUCKETS_MOD_TAB).durability(0)
     ));
     public static final RegistryObject<Item> OBSIDIAN_BUCKET = ITEMS.register("obsidian_bucket", () -> new UniversalBucketItem(
-            new UniversalBucketItem.Properties().tab(ModItemTabs.MORE_BUCKETS_MOD_TAB).durability(500)
+            new UniversalBucketItem.Properties().tab(ModItemTabs.MORE_BUCKETS_MOD_TAB).durability(0)
     ));
 
     //Unusual
     public static final RegistryObject<Item> UNFIRED_CLAY_BUCKET = ITEMS.register("unfired_clay_bucket", () -> new UniversalBucketItem(
-            new UniversalBucketItem.Properties().tab(ModItemTabs.MORE_BUCKETS_MOD_TAB).durability(3).upperCrackingTemperature(1000).lowerCrackingTemperature(0).disableEntityObtaining().disableBlockObtaining().disableMilking()
+            new UniversalBucketItem.Properties().tab(ModItemTabs.MORE_BUCKETS_MOD_TAB).durability(2).upperCrackingTemperature(1000).lowerCrackingTemperature(0).disableEntityObtaining().disableBlockObtaining().disableMilking()
     ));
     public static final RegistryObject<Item> CERAMIC_BUCKET = ITEMS.register("ceramic_bucket", () -> new UniversalBucketItem(
-            new UniversalBucketItem.Properties().tab(ModItemTabs.MORE_BUCKETS_MOD_TAB).durability(150)
+            new UniversalBucketItem.Properties().tab(ModItemTabs.MORE_BUCKETS_MOD_TAB).durability(0)
     ));
 }
